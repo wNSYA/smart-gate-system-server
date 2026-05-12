737d779d0d97998f44e0f07347246076e63d81d5
natanael-ss <naelsteven2304@gmail.com>
2026-05-12 13:09:33 +0700

    fix: delete junk prisma migrations file

--- prisma/migrations/20260510194039_employee_last_synced_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "employee" ADD COLUMN     "last_synced_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- prisma/migrations/20260512041945_optionalilng_door_no/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "eventRecord" ALTER COLUMN "doorNo" DROP NOT NULL;
